1581d80e4e2af474ab933e41dd7cb3008ff707d4
zhangxiaocong <58288531+bencong55@users.noreply.github.com>
2020-04-24 12:21:47 +0800

    增加vant支持

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,12 @@ import 'vant/lib/index.css';
 import 'vant/lib/button/style';
 import { Button } from 'vant';
 import Antd from 'ant-design-vue'
+import { Tab, Tabs } from 'vant';
 Vue.use(Vuex)
 Vue.use(Antd)
 Vue.use(Button);
+Vue.use(Tab);
+Vue.use(Tabs);
 
 export default new Vuex.Store({
   state: {
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-tabs v-model="active">
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+
   </div>
 </template>
 <script lang="ts">
   import { Component, Vue } from "vue-property-decorator";
   import vant from "vant"
+
   @Component
   export default class About extends Vue{
     test(){
       vant.Toast('Message');
+    }
+    data() {
+      return {
+        active: 2,
+      }
     }
   }
 
